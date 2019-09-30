01541e604329a917258dcd67e8afafc578aa8ed4
Pereira Ricardo <nunocodex@gmail.com>
2019-09-30 16:43:18 +0200

    lavender: Installs gsi keys into ramdisk, to boot a GSI with verified boot

--- device.mk
+++ device.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
