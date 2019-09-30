e6b939a95871c866a77e59a590373d95898f92d9
Pereira Ricardo <nunocodex@gmail.com>
2019-09-30 16:42:55 +0200

    lavender: Enable updating of APEXes

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
 # Enable updating of APEXes
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
